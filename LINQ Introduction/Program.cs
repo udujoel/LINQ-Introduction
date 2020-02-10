3d70c14fcb7e94cb46a6f5a767fedf4908b288da
Joell <amamihechi.udu@decagon.dev>
2020-02-10 04:30:05 +0100

    Add project files.

---
+++ LINQ Introduction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace LINQ_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\windows";
+            ShowLargeFIlesWithoutLINQ(path);
+        }
+
+        private static void ShowLargeFIlesWithoutLINQ(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            var files = directory.GetFiles();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+        }
+    }
+}
