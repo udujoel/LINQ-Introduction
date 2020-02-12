40fc90e152e87f3777ea1c4fe7dc2b5f015682c0
Joell <amamihechi.udu@decagon.dev>
2020-02-12 07:25:16 +0100

    Features prject to test out C# features

---
+++ Features/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Features
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee[] developers = new Employee[]
+            {
+                new Employee {Id = 1, Name = "Kent"},
+                new Employee {Id = 2, Name = "Willis"},
+                new Employee {Id = 3, Name = "Burrows"}
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee{Id = 1, Name = "Walrus"},
+                new Employee{Id = 2,Name = "Tony"}
+            };
+
+
+
+        }
+    }
+}
