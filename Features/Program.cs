761e64e356356c999a7182439fe1572ca7e42ce4
Joell <amamihechi.udu@decagon.dev>
2020-02-12 07:47:03 +0100

    Using IEnumerable

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Features
 {
@@ -7,21 +8,24 @@ namespace Features
         static void Main(string[] args)
         {
 
-            Employee[] developers = new Employee[]
+            var developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Kent"},
                 new Employee {Id = 2, Name = "Willis"},
                 new Employee {Id = 3, Name = "Burrows"}
             };
 
-            List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee{Id = 1, Name = "Walrus"},
                 new Employee{Id = 2,Name = "Tony"}
             };
 
 
-
+            foreach (var person in developers)
+            {
+                Console.WriteLine($"\tID: {person.Id,2}\t Name: {person.Name,2}");
+            }
         }
     }
 }
